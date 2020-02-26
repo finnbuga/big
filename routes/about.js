a55cb44715fa0b9ac66d948838c671a55285cb2d
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-26 15:33:24 +0000

    feat: add About page

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
+/* GET About page. */
 router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+  res.render("index", { title: "This page is about me" });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Performance tips for a snapy UI" });
 });
 
 module.exports = router;
