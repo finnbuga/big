11017e188fa4d0a98ac571847c6fbe99402ab6b5
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-27 09:01:14 +0000

    feat: add images to the portfolio

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,9 +1,25 @@
 var express = require("express");
 var router = express.Router();
 
+const breakpoint = "1024px";
+const pics = [
+  [
+    "/images/Mobile/Mobile-Image-One.jpg",
+    "/images/Desktop/Desktop-Image-One.jpg"
+  ],
+  [
+    "/images/Mobile/Mobile-Image-Two.jpg",
+    "/images/Desktop/Desktop-Image-Two.jpg"
+  ],
+  [
+    "/images/Mobile/Mobile-Image-Three.jpg",
+    "/images/Desktop/Desktop-Image-Three.jpg"
+  ]
+];
+
 /* GET Portfolio page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Portfolio" });
+  res.render("portfolio", { title: "Portfolio", pics, breakpoint });
 });
 
 module.exports = router;
