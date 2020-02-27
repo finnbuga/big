142e4f8993e810004753aa4af024d112eeaa8cf8
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-27 07:37:37 +0000

    feat: add Portfolio empty page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var aboutRouter = require("./routes/about");
+var portfolioRouter = require("./routes/portfolio");
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/about", aboutRouter);
+app.use("/portfolio", portfolioRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
